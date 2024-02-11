c0fc7ecf60f8800289f9183239a15f3cb02ed8b0
alainahebie <alainahebie@gmail.com>
2024-02-11 19:38:08 +0100

    optimize strategy

--- prologue/example.py
+++ prologue/example.py
@@ -1,5 +1,5 @@
 import warnings
-warnings.filterwarnings("ignore", category=FutureWarning, module='backtesting.*')
+warnings.filterwarnings("ignore")
 from backtesting import Backtest, Strategy
 from backtesting.test import GOOG
 from backtesting.lib import crossover
@@ -31,7 +31,12 @@ class RsiOscillator(Strategy):
 
 bt = Backtest(GOOG, RsiOscillator, cash= 10_000)
 
-stats = bt.run()
+stats = bt.optimize(
+    upper_bound = range(50, 85, 5),
+    lower_bound = range(10, 45, 5),
+    rsi_window = range(10, 30, 2),
+    maximize= 'Sharpe Ratio'
+)
 print(stats)
 
 bt.plot()
