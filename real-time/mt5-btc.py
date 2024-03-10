6e07065712ff3688b2058001e8b95d0104d378e7
alainahebie <alainahebie@gmail.com>
2024-03-10 16:11:41 +0100

    connect to mt5

---
+++ real-time/mt5-btc.py
@@ -0,0 +1,28 @@
+import MetaTrader5 as mt5
+# display data on the MetaTrader 5 package
+print("MetaTrader5 package author: ",mt5.__author__)
+print("MetaTrader5 package version: ",mt5.__version__)
+ 
+# establish connection to the MetaTrader 5 terminal
+if not mt5.initialize():
+    print("initialize() failed, error code =",mt5.last_error())
+    quit()
+ 
+# display data on MetaTrader 5 version
+print(mt5.version())
+# now connect to another trading account specifying the password
+account=26004346
+authorized=mt5.login(account, password=";5k|QTT5",server = "mt5live.fxopen.net")
+if authorized:
+    # display trading account data 'as is'
+    print(mt5.account_info())
+    # display trading account data in the form of a list
+    print("Show account_info()._asdict():")
+    account_info_dict = mt5.account_info()._asdict()
+    for prop in account_info_dict:
+        print("  {}={}".format(prop, account_info_dict[prop]))
+else:
+    print("failed to connect at account #{}, error code: {}".format(account, mt5.last_error()))
+ 
+# shut down connection to the MetaTrader 5 terminal
+mt5.shutdown()
